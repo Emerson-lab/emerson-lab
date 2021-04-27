d34dcbb03538290d8d46df6c6360d83346826cc7
Emerson <trinddade29@gmail.cm>
2021-04-26 23:30:15 -0300

    EP01

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-
+import useAuth from './../hooks/useAuth';
 
 export default function Home() {
+
+  const { user, signin } = useAuth();
+  console.log('user', user);
+
   return (
     <div>
       <h2>Welcome to Next.js!</h2>
+      <button type="button" onClick={() => signin()}>Entrar com github</button>
     </div>
   )
 }
